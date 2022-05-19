43d14c58cd97a04f6458f7d4af266107707e89dd
Dariusz Rybicki <dariusz@elixxir.io>
2022-05-19 09:36:31 +0200

    Add GroupInfoTests to XXModelTests target

---
+++ Tests/XXModelsTests/GroupInfoTests.swift
@@ -0,0 +1,25 @@
+import XCTest
+@testable import XXModels
+
+final class GroupInfoTests: XCTestCase {
+  func testId() {
+    let leader = Contact(
+      id: "contact-id".data(using: .utf8)!
+    )
+
+    let group = Group(
+      id: "group-id".data(using: .utf8)!,
+      name: "",
+      leaderId: leader.id,
+      createdAt: Date()
+    )
+
+    let groupInfo = GroupInfo(
+      group: group,
+      leader: leader,
+      members: []
+    )
+
+    XCTAssertEqual(groupInfo.id, group.id)
+  }
+}
