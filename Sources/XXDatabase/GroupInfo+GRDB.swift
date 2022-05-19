1107085606b87df39b7daae13ef07087e85a20bc
Dariusz Rybicki <dariusz@elixxir.io>
2022-05-19 10:17:51 +0200

    Fix GroupInfo.request

--- Sources/XXDatabase/GroupInfo+GRDB.swift
+++ Sources/XXDatabase/GroupInfo+GRDB.swift
@@ -2,11 +2,11 @@ import GRDB
 import XXModels
 
 extension GroupInfo: FetchableRecord {
-  public static func request(_ query: Query, _ order: Order) -> QueryInterfaceRequest<GroupMember> {
+  public static func request(_ query: Query, _ order: Order) -> QueryInterfaceRequest<GroupInfo> {
     var request = Group
       .including(required: Group.Association.leader)
       .including(all: Group.Association.members)
-      .asRequest(of: GroupMember.self)
+      .asRequest(of: GroupInfo.self)
 
     // TODO: handle query
 
