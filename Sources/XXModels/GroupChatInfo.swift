19c730df93d8e7fc5a6529da66dacd8e5c2f0404
Dariusz Rybicki <dariusz@elixxir.io>
2022-05-23 10:20:56 +0200

    Add GroupChatInfo model

---
+++ Sources/XXModels/GroupChatInfo.swift
@@ -0,0 +1,29 @@
+import Combine
+
+public struct GroupChatInfo: Identifiable, Equatable, Codable {
+  public typealias ID = Group.ID
+
+  public init(
+    group: Group,
+    lastMessage: Message?,
+    unreadCount: Int
+  ) {
+    self.group = group
+    self.lastMessage = lastMessage
+    self.unreadCount = unreadCount
+  }
+
+  public var id: ID { group.id }
+  public var group: Group
+  public var lastMessage: Message?
+  public var unreadCount: Int
+}
+
+extension GroupChatInfo {
+  public typealias Fetch = (Query) throws -> [GroupChatInfo]
+  public typealias FetchPublisher = (Query) -> AnyPublisher<[GroupChatInfo], Error>
+
+  public struct Query: Equatable {
+    public init() {}
+  }
+}
---
+++ Tests/XXModelsTests/GroupChatInfoTests.swift
@@ -0,0 +1,25 @@
+import XCTest
+@testable import XXModels
+
+final class GroupChatInfoTests: XCTestCase {
+  func testId() {
+    let leader = Contact(
+      id: "leader-contact-id".data(using: .utf8)!
+    )
+
+    let group = Group(
+      id: "group-id".data(using: .utf8)!,
+      name: "group-name",
+      leaderId: leader.id,
+      createdAt: Date(timeIntervalSince1970: 1234)
+    )
+
+    let chatInfo = GroupChatInfo(
+      group: group,
+      lastMessage: nil,
+      unreadCount: 0
+    )
+
+    XCTAssertEqual(chatInfo.id, group.id)
+  }
+}
