4bad981fc07623a0b65a1f0b265cf17adcd740cf
Dariusz Rybicki <dariusz@elixxir.io>
2022-05-25 15:11:53 +0200

    Add docs for operation interfaces

--- Sources/XXModels/Operations/FetchPublisher.swift
+++ Sources/XXModels/Operations/FetchPublisher.swift
@@ -1,10 +1,20 @@
 import Combine
 
+/// Model fetch operation publisher
+///
+/// - Takes `Query` that should be used for the fetch.
+/// - Returns publisher that emits array of `Model`s whenever they change.
+/// - The publisher completes with `Error` on operation failure.
 public struct FetchPublisher<Model, Query> {
+  /// Instantiate operation
+  ///
+  /// - Parameters:
+  ///   - run: Closure that returns operation publisher
   public init(run: @escaping (Query) -> AnyPublisher<[Model], Error>) {
     self.run = run
   }
 
+  /// Closure that returns operation publisher
   public var run: (Query) -> AnyPublisher<[Model], Error>
 
   public func callAsFunction(_ query: Query) -> AnyPublisher<[Model], Error> {
