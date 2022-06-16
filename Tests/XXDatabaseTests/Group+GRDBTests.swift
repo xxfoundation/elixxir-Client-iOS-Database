8aad9875885b814aea403a6186f02c08543f78a9
Dariusz Rybicki <dariusz@elixxir.io>
2022-06-16 11:42:08 +0200

    Refactor

--- Tests/XXDatabaseTests/Group+GRDBTests.swift
+++ Tests/XXDatabaseTests/Group+GRDBTests.swift
@@ -108,33 +108,38 @@ final class GroupGRDBTests: XCTestCase {
 
     // Fetch all groups:
 
-    XCTAssertNoDifference(try db.fetchGroups(Group.Query(sortBy: .createdAt())), [
-      groupA, groupB, groupC,
-    ])
+    XCTAssertNoDifference(
+      try db.fetchGroups(Group.Query(sortBy: .createdAt())),
+      [groupA, groupB, groupC]
+    )
 
-    XCTAssertNoDifference(try db.fetchGroups(Group.Query(sortBy: .createdAt(desc: true))), [
-      groupC, groupB, groupA,
-    ])
+    XCTAssertNoDifference(
+      try db.fetchGroups(Group.Query(sortBy: .createdAt(desc: true))),
+      [groupC, groupB, groupA]
+    )
 
     // Fetch groups with messages:
 
-    XCTAssertNoDifference(try db.fetchGroups(Group.Query(withMessages: true, sortBy: .createdAt())), [
-      groupA,
-    ])
+    XCTAssertNoDifference(
+      try db.fetchGroups(Group.Query(withMessages: true, sortBy: .createdAt())),
+      [groupA]
+    )
 
     // Fetch groups without messages:
 
-    XCTAssertNoDifference(try db.fetchGroups(Group.Query(withMessages: false, sortBy: .createdAt())), [
-      groupB, groupC,
-    ])
+    XCTAssertNoDifference(
+      try db.fetchGroups(Group.Query(withMessages: false, sortBy: .createdAt())),
+      [groupB, groupC]
+    )
 
     // Fetch groups with auth status `participating` or `pending`:
 
-    XCTAssertNoDifference(try db.fetchGroups(Group.Query(
-      authStatus: [.participating, .pending],
-      sortBy: .createdAt()
-    )), [
-      groupA, groupB,
-    ])
+    XCTAssertNoDifference(
+      try db.fetchGroups(Group.Query(
+        authStatus: [.participating, .pending],
+        sortBy: .createdAt()
+      )),
+      [groupA, groupB]
+    )
   }
 }
