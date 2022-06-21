91153cc427ca8790602e62a57994d394620d1b0c
Dariusz Rybicki <dariusz@elixxir.io>
2022-06-22 00:29:44 +0200

    Refactor

--- Sources/XXDatabase/Operations/BulkUpdate+GRDB.swift
+++ Sources/XXDatabase/Operations/BulkUpdate+GRDB.swift
@@ -3,19 +3,17 @@ import GRDB
 import XXModels
 
 extension BulkUpdate {
-  static func grdb<Record, Query, Update>(
+  static func grdb<Record, Query, Assignments>(
     _ writer: DatabaseWriter,
     _ queue: DispatchQueue,
     _ request: @escaping (Query) -> QueryInterfaceRequest<Record>,
-    _ assignments: @escaping (Update) -> [ColumnAssignment]
-  ) -> BulkUpdate<Query, Update>
-  where Record: MutablePersistableRecord,
-        Record: FetchableRecord
-  {
-    BulkUpdate<Query, Update> { query, update in
+    _ columnAssignments: @escaping (Assignments) -> [ColumnAssignment]
+  ) -> BulkUpdate<Query, Assignments>
+  where Record: FetchableRecord {
+    BulkUpdate<Query, Assignments> { query, assignments in
       try queue.sync {
         try writer.write { db in
-          try request(query).updateAll(db, assignments(update))
+          try request(query).updateAll(db, columnAssignments(assignments))
         }
       }
     }
--- Sources/XXModels/Operations/BulkUpdate.swift
+++ Sources/XXModels/Operations/BulkUpdate.swift
@@ -1,29 +1,29 @@
 /// Models bulk update operation
 ///
 /// - Updates all `Model` objects that matches provided query.
-/// - Performs updates defined by provided `Update` struct.
+/// - Performs updates defined by provided `Assignments` struct.
 /// - Returns number of updated objects.
-public struct BulkUpdate<Query, Update> {
+public struct BulkUpdate<Query, Assignments> {
   /// Instantiate operation
   ///
   /// - Parameter run: Closure that performs the operation
-  public init(run: @escaping (Query, Update) throws -> Int) {
+  public init(run: @escaping (Query, Assignments) throws -> Int) {
     self.run = run
   }
 
   /// Closure that performs the operation
-  public var run: (Query, Update) throws -> Int
+  public var run: (Query, Assignments) throws -> Int
 
   @discardableResult
-  public func callAsFunction(_ query: Query, _ update: Update) throws -> Int {
+  public func callAsFunction(_ query: Query, _ update: Assignments) throws -> Int {
     try run(query, update)
   }
 }
 
 #if DEBUG
 extension BulkUpdate {
-  public static func failing<Query, Update>() -> BulkUpdate<Query, Update> {
-    BulkUpdate<Query, Update> { _, _ in fatalError() }
+  public static func failing<Query, Assignments>() -> BulkUpdate<Query, Assignments> {
+    BulkUpdate<Query, Assignments> { _, _ in fatalError() }
   }
 }
 #endif
