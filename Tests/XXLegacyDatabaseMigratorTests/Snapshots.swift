d64a597fca31de251c5db6d81c247063721c987f
Dariusz Rybicki <dariusz@elixxir.io>
2022-06-21 18:26:05 +0200

    Implement new database snapshot

--- Tests/XXLegacyDatabaseMigratorTests/Snapshots.swift
+++ Tests/XXLegacyDatabaseMigratorTests/Snapshots.swift
@@ -1,12 +1,33 @@
 import GRDB
 import XXModels
+@testable import XXDatabase
 
 struct DatabaseSnapshot: Equatable, Codable {
-
+  var contacts: [XXModels.Contact]
+  var groups: [XXModels.Group]
+  var groupMembers: [XXModels.GroupMember]
+  var fileTransfers: [XXModels.FileTransfer]
+  var messages: [XXModels.Message]
 }
 
 extension DatabaseSnapshot {
   static func make(with reader: DatabaseReader) throws -> DatabaseSnapshot {
-    DatabaseSnapshot()
+    DatabaseSnapshot(
+      contacts: try reader.read(XXModels.Contact
+        .order(XXModels.Contact.Column.createdAt)
+        .fetchAll(_:)),
+      groups: try reader.read(XXModels.Group
+        .order(XXModels.Group.Column.createdAt)
+        .fetchAll(_:)),
+      groupMembers: try reader.read(XXModels.GroupMember
+        .order(XXModels.GroupMember.Column.groupId)
+        .fetchAll(_:)),
+      fileTransfers: try reader.read(XXModels.FileTransfer
+        .order(XXModels.FileTransfer.Column.createdAt)
+        .fetchAll(_:)),
+      messages: try reader.read(XXModels.Message
+        .order(XXModels.Message.Column.date)
+        .fetchAll(_:))
+    )
   }
 }
