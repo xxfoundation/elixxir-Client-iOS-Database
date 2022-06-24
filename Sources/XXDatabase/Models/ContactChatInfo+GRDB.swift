601ce8c05def9376fe2f2c4bd04aa29337505f54
Dariusz Rybicki <dariusz@elixxir.io>
2022-06-24 21:56:33 +0200

    Add auth status filter to ContactChatInfo.Query

--- Sources/XXDatabase/Models/ContactChatInfo+GRDB.swift
+++ Sources/XXDatabase/Models/ContactChatInfo+GRDB.swift
@@ -9,34 +9,45 @@ extension ContactChatInfo: FetchableRecord {
   }
 
   static func request(_ query: Query) -> AdaptedFetchRequest<SQLRequest<ContactChatInfo>> {
-    SQLRequest(
-      sql: """
-        SELECT
-          -- All contact columns:
-          c2.*,
-          -- Unread messages count column:
-          COUNT(CASE WHEN m.isUnread THEN 1 END) AS unreadCount,
-          -- All message columns:
-          m.*,
-          -- Latest message date column:
-          MAX(m.date) AS date
-        FROM
-          messages m
-        INNER JOIN contacts c1
-          ON c1.id IN (m.senderId, m.recipientId)
-        INNER JOIN contacts c2
-          ON c2.id IN (m.senderId, m.recipientId)
-          AND c1.id <> c2.id
-        WHERE
-          c1.id = :userId
-        GROUP BY
-          c2.id
-        ORDER BY
-          date DESC;
-        """,
-      arguments: [
-        "userId": query.userId
-      ]
+    var whereQuery: [String] = ["c1.id = :userId"]
+    var arguments: StatementArguments = ["userId": query.userId]
+
+    if let authStatus = query.authStatus {
+      let authStatusArgumentNames = (0..<authStatus.count).map { ":authStatus\($0)" }
+      whereQuery.append("c2.authStatus IN (\(authStatusArgumentNames.joined(separator: ", ")))")
+      authStatus.enumerated().forEach { index, status in
+        _ = arguments.append(contentsOf: ["authStatus\(index)": status.rawValue])
+      }
+    }
+
+    let sql = """
+      SELECT
+        -- All contact columns:
+        c2.*,
+        -- Unread messages count column:
+        COUNT(CASE WHEN m.isUnread THEN 1 END) AS unreadCount,
+        -- All message columns:
+        m.*,
+        -- Latest message date column:
+        MAX(m.date) AS date
+      FROM
+        messages m
+      INNER JOIN contacts c1
+        ON c1.id IN (m.senderId, m.recipientId)
+      INNER JOIN contacts c2
+        ON c2.id IN (m.senderId, m.recipientId)
+        AND c1.id <> c2.id
+      WHERE
+        \(whereQuery.joined(separator: "\n  AND "))
+      GROUP BY
+        c2.id
+      ORDER BY
+        date DESC;
+      """
+
+    return SQLRequest(
+      sql: sql,
+      arguments: arguments
     )
     .adapted { db in
       let adapters = try splittingRowAdapters(columnCounts: [
--- Tests/XXDatabaseTests/ContactChatInfo+GRDBTests.swift
+++ Tests/XXDatabaseTests/ContactChatInfo+GRDBTests.swift
@@ -148,4 +148,115 @@ final class ContactChatInfoGRDBTests: XCTestCase {
       ]
     )
   }
+
+  func testFetchingByContactAuthStatus() throws {
+    // Mock up contacts:
+
+    let contactA = try db.saveContact(.stub("A"))
+    let contactB = try db.saveContact(.stub("B", authStatus: .friend))
+    let contactC = try db.saveContact(.stub("C", authStatus: .hidden))
+    let contactD = try db.saveContact(.stub("D", authStatus: .stranger))
+
+    // Mock up conversation between contact A and B:
+
+    try db.saveMessage(.stub(
+      from: contactA,
+      to: contactB,
+      at: 1,
+      isUnread: false
+    ))
+
+    try db.saveMessage(.stub(
+      from: contactB,
+      to: contactA,
+      at: 2,
+      isUnread: true
+    ))
+
+    let lastMessage_betweenAandB_at3 = try db.saveMessage(.stub(
+      from: contactA,
+      to: contactB,
+      at: 3,
+      isUnread: true
+    ))
+
+    // Mock up conversation between contact A and C:
+
+    try db.saveMessage(.stub(
+      from: contactA,
+      to: contactC,
+      at: 4,
+      isUnread: false
+    ))
+
+    let lastMessage_betweenAandC_at5 = try db.saveMessage(.stub(
+      from: contactC,
+      to: contactA,
+      at: 5,
+      isUnread: true
+    ))
+
+    // Mock up conversation between contact A and D:
+
+    try db.saveMessage(.stub(
+      from: contactA,
+      to: contactD,
+      at: 6,
+      isUnread: false
+    ))
+
+    let lastMessage_betweenAandD_at7 = try db.saveMessage(.stub(
+      from: contactD,
+      to: contactA,
+      at: 7,
+      isUnread: false
+    ))
+
+    // Fetch contact chat infos for contact A, filtered by other contact auth status:
+
+    XCTAssertNoDifference(
+      try db.fetchContactChatInfos(ContactChatInfo.Query(
+        userId: contactA.id,
+        authStatus: [.friend, .stranger]
+      )),
+      [
+        ContactChatInfo(
+          contact: contactD,
+          lastMessage: lastMessage_betweenAandD_at7,
+          unreadCount: 0
+        ),
+        ContactChatInfo(
+          contact: contactB,
+          lastMessage: lastMessage_betweenAandB_at3,
+          unreadCount: 2
+        ),
+      ]
+    )
+
+    // Fetch contact chat infos for contact A, regardless other contact auth status:
+
+    XCTAssertNoDifference(
+      try db.fetchContactChatInfos(ContactChatInfo.Query(
+        userId: contactA.id,
+        authStatus: nil
+      )),
+      [
+        ContactChatInfo(
+          contact: contactD,
+          lastMessage: lastMessage_betweenAandD_at7,
+          unreadCount: 0
+        ),
+        ContactChatInfo(
+          contact: contactC,
+          lastMessage: lastMessage_betweenAandC_at5,
+          unreadCount: 1
+        ),
+        ContactChatInfo(
+          contact: contactB,
+          lastMessage: lastMessage_betweenAandB_at3,
+          unreadCount: 2
+        ),
+      ]
+    )
+  }
 }
