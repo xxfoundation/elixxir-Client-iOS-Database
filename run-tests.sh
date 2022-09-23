89708bf0d23dd69df0a96f3181326524f93e0135
Dariusz Rybicki <dariusz@elixxir.io>
2022-09-23 17:46:30 +0200

    Remove xcbeautify

--- run-tests.sh
+++ run-tests.sh
@@ -4,12 +4,12 @@ set -e
 if [ "$1" = "macos" ]; then
 
   echo "\n\033[1;32m▶ Running package tests on macOS...\033[0m"
-  set -o pipefail && swift test 2>&1 | ./xcbeautify
+  swift test
 
 elif [ "$1" = "ios" ]; then
 
   echo "\n\033[1;32m▶ Running package tests on iOS Simulator...\033[0m"
-  set -o pipefail && xcodebuild -scheme 'xx-client-ios-db-Package' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=16.0,name=iPhone 14' test | ./xcbeautify
+  xcodebuild -scheme 'xx-client-ios-db-Package' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=16.0,name=iPhone 14' test
 
 else
 
